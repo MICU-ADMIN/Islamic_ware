0b7789fc882d353606db8a72c1054d9f527526fd
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-31 19:53:04 +0000

    working perfectly

---
+++ packages/story/stories/emailRegister.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { EmailRegister } from "../../ui/src/email-register";
+
+export default {
+  title: "UI/EmailRegister",
+  component: EmailRegister,
+};
+
+export const EmailRegisterComponent = () => (
+  // @ts-ignore
+  <EmailRegister />
+);
